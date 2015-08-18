cb90e599b9c831a4e0591a537495e787fb5bedac
odekakewaqwaq <odekakewaqwaq@gmail.com>
2015-08-18 14:41:13 +0000

    Implementation of the age validation

--- app/models/message.rb
+++ app/models/message.rb
@@ -3,4 +3,6 @@ class Message < ActiveRecord::Base
     validates:name,length:{maximum:20},presence:true
     #内容は入力必須且つ2文字以上30文字以下
     validates:body,length:{minimum:2,maximum:30},presence:true
+    #内容は入力必須且つ0以上200以下
+    validates:age,numericality:{greater_than_or_equal_to:0,less_than:200},presence:true
 end
